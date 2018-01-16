a9addccb9f8d7f61700e7f339e2d1677fa777e4a
assig <assigdev@gmail.com>
2018-01-17 01:17:05 +0300

    + Question, Answer and Tag models in qa app

--- apps/qa/models.py
+++ apps/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=40)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers')
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    is_true = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "answer {0} for question: {1}".format(self.id, self.question.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=24)
+    slug = models.SlugField(max_length=24)
+
+    def __str__(self):
+        return self.title
